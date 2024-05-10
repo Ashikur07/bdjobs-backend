eb079e6b517d2338ac4c24ffa9591ba0f3c8484d
Ashikur07 <ashik.ict.iu@gmail.com>
2024-05-09 22:02:27 -0500

    complete basic setup

---
+++ index.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const cors = require('cors');
+// const jwt = require('jsonwebtoken');
+// const cookieParser = require('cookie-parser');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config()
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+// app.use(cookieParser());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ny1xaie.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        
+        // database collection 
+        const jobsCollection = client.db('assignment_11').collection('Jobs');
+
+        
+    
+         // service related api
+         // get all jobs
+         app.get('/jobs', async (req, res) => {
+            let query = {};
+            if (req.query?.job_type) {
+                query = { job_type: req.query.job_type }
+            }
+            const result = await jobsCollection.find(query).toArray();
+            res.send(result);
+        })
+
+
+
+        // Send a ping to confirm a successful connection
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('doctor is running')
+})
+
+app.listen(port, () => {
+    console.log(`Car Doctor Server is running on port ${port}`)
+})
